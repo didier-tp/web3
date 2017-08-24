7a5df8e4e655c11f1ed31f1349b6dad81f0ce7d9
didier <didier-defrance@wanadoo.fr>
2017-08-24 12:23:05 +0200

    ajout_tableau_appendChild

--- dom_crud.js
+++ dom_crud.js
@@ -24,30 +24,29 @@ function init(){
 
 function lireValeursSaisies(){
 	if(!prod){
-		//prod = new Product();//si fonction constructeur
-		// { id: 0 , nom : "?"} est
-		//une expression littérale d'objet javascript
-		//avec des valeurs par défaut
-		//(très proche du format JSON)
-		prod = { id : 0 ,
-		         nom : "?",
-				 prix : 0 ,
-				 couleur : "black"};
+		prod = { id : 0 ,  nom : "?",
+				 prix : 0 ,	 couleur : "black"};
 	}
 	prod.id = inputId.value;
-	prod.nom = inputNom.value;
-	//...
+	prod.nom = inputNom.value;	//...
 }
-
 function logObject(obj){
-	//console.log(obj.toString());
 	for( indexPropriete in obj){
 		console.log(obj[indexPropriete]);
 	}
 }
-
 function ajouter(event){
  //alert("click sur " + event.target.value);
  lireValeursSaisies();
  logObject(prod);
+ ajouterLigneDansTableau();
+}
+
+function ajouterLigneDansTableau(){
+var newTr = document.createElement("tr");
+tabProduits.appendChild(newTr);
+var newTd1 = document.createElement("td");
+newTr.appendChild(newTd1);
+newTd1.innerHTML=prod.id;
+//etc , etc
 }
