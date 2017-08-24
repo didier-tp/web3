47e6b790d18eb11f107753c4f89ff78e0d50ac03
didier <didier-defrance@wanadoo.fr>
2017-08-24 11:00:23 +0200

    onload

--- dom_crud.js
+++ dom_crud.js
@@ -1,5 +1,15 @@
-function ajouter(){
-	var ... = 
-	  document.getElementById(....);  
+function init(){
+	var btnAdd = 
+	  //document.getElementById("btnAdd"); 
+	  document.querySelector("#btnAdd");
+    btnAdd.style.fontWeight="bold";
+
+    btnAdd.addEventListener("click",
+	                        ajouter);
     	 
+}
+
+function ajouter(event){
+	alert("click sur " + 
+	       event.target.value);
 }
